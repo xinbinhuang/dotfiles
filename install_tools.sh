6a6bd75c44b9d136720554b75d7c121e3ffc78a6
Xinbin Huang <bin.huangxb@gmail.com>
2020-06-02 18:08:42 -0700

    Clean up scripts

--- install_tools.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Anaconda and Python
-ANACONDA_URL=$(curl -s https://www.anaconda.com/distribution/ | grep -oP "https:\/\/repo.anaconda.com\/archive\/Anaconda3-[0-9\.]*-Linux-.*64\.sh(?=\"\s)")
-ANACONDA_FILE="install_anaconda.sh"
-wget ${ANACONDA_URL} -O ${ANACONDA_FILE}
-bash ${ANACONDA_FILE}
-rm ${ANACONDA_FILE}
