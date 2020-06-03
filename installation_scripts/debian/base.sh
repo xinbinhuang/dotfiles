619a553e5cc35c9cae986f152531ae3dd3df29a9
Xinbin Huang <bin.huangxb@gmail.com>
2020-06-02 23:28:32 -0700

    Add build deps for python
    will need clearn up later

--- installation_scripts/debian/base.sh
+++ installation_scripts/debian/base.sh
@@ -17,3 +17,8 @@ apps=(
 )
 
 apt update -yyy && apt install -yyy ${apps[@]}
+
+# Build dependencies for building Python from source using pyenv
+# apt install -y make build-essential libssl-dev zlib1g-dev \
+#     libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
+#     libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl
