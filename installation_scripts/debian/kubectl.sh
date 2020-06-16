ecb4e5791ec67eb65af4bc1d1d384ee506dd1fd1
Xinbin Huang <bin.huangxb@gmail.com>
2020-06-16 16:03:55 -0700

    Add krew installation

--- installation_scripts/debian/kubectl.sh
+++ installation_scripts/debian/kubectl.sh
@@ -1,3 +1,18 @@
 #! /usr/bin/bash
 
+EXPORT_FILE=$ZSH_CUSTOM/exports.zsh
+
 sudo apt install kubectl -yyy
+
+# kubectl plugin: krew
+(
+  set -x; cd "$(mktemp -d)" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/krew.{tar.gz,yaml}" &&
+  tar zxvf krew.tar.gz &&
+  KREW=./krew-"$(uname | tr '[:upper:]' '[:lower:]')_amd64" &&
+  "$KREW" install --manifest=krew.yaml --archive=krew.tar.gz &&
+  "$KREW" update
+)
+
+echo '\n# kubectl plugin: krew' >> $EXPORT_FILE
+echo 'export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"' >> $EXPORT_FILE
